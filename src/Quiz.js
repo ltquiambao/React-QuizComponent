58666e4245e65308f798f7befaf6cef3c4945745
lorenzo <lorenzotquiambao@gmail.com>
2021-04-04 19:02:19 -0400

    Task 7 - Resetting the Quiz - Correction

--- src/Quiz.js
+++ src/Quiz.js
@@ -26,14 +26,15 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd 
+            resetClickHandler={this.handleResetClick.bind(this)}
+          />
         ) : (
           <QuizQuestion
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
             showNextQuestionHandler={this.showNextQuestion.bind(this)}
-            resetClickHandler={this.handleResetClick.bind(this)}
           />
         )}
       </div>
